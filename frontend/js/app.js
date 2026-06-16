d1285dfe5336fe30c30768e399b3e927c8b3aea2
RajGehlot28 <rajgehlot2007@gmail.com>
2026-06-16 22:48:54 +0530

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -55,7 +55,7 @@ async function sendMessage() {
   showLoading();
 
   try {
-    const response = await fetch("https://college-notes-rag-system.onrender.com/ask", {
+    const response = await fetch("http://127.0.0.1:8000/ask", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
